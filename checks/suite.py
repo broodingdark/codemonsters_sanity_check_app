c798707bc530ac4b0e1c3d4afaa1e8d222e47daf
broodingdark <ivan.ivanov@solarwinds.com>
2021-06-30 15:28:10 +0300

    add shortcuts to config fields

--- checks/suite.py
+++ checks/suite.py
@@ -14,3 +14,14 @@ def get_config():
 
 
 CONFIG = get_config()
+USERNAME = CONFIG["credentials"]["username"]
+PASSWORD = CONFIG["credentials"]["password"]
+ROOT = CONFIG["endpoints"]["root"]
+PAYMENT_TRANSACTIONS_ENDPOINT = CONFIG["endpoints"]["payment_transactions"]
+SALE_REQUEST_DATA = CONFIG["test_data"]["valid_sale_transaction"]
+VOID_REQUEST_DATA = CONFIG["test_data"]["valid_void_transaction"]
+SALE_SUCCESS_RESPONSE_DATA = CONFIG["expected_responses"]["sale_transaction_success"]
+VOID_SUCCESS_RESPONSE_DATA = CONFIG["expected_responses"]["void_transaction_success"]
+SALE_ERROR_RESPONSE_DATA = CONFIG["expected_responses"]["sale_transaction_error"]
+VOID_ERROR_RESPONSE_DATA = CONFIG["expected_responses"]["void_transaction_error"]
+COMPLETE_PAYMENT_TRANSACTIONS_ENDPOINT = ROOT + PAYMENT_TRANSACTIONS_ENDPOINT
