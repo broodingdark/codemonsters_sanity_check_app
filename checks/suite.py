5d1beff006342278d35db27c3cde5907c54d0cb9
broodingdark <ivan.ivanov@solarwinds.com>
2021-06-30 15:43:13 +0300

    add generic function to send requests and validate responses

--- checks/suite.py
+++ checks/suite.py
@@ -1,5 +1,7 @@
 import json
 from pathlib import Path
+from requests import post
+from requests.auth import HTTPBasicAuth
 
 
 CONFIG_NAME = "checks/config.json"
@@ -33,3 +35,33 @@ def setup_module(module):
 
 def teardown_module(module):
     return Path(CONFIG_NAME).unlink()
+
+
+def send_and_validate_transaction(endpoint, data, username, password, expected_code, expected_data):
+    """
+    Main function ot use for polling the API and validating the response.
+    Return value depends on the expected_data argument: if JSON is expected,
+    function tries to return JSON; if text is expected, function tries to return text.
+    :param endpoint: full URL endpoint
+    :param data: JSON data used for the request
+    :param username: username for auth
+    :param password: password for auth
+    :param expected_code: expected response code
+    :param expected_data: expected response data
+    :return: str or dict
+    """
+    response = post(endpoint, json=data, auth=HTTPBasicAuth(username, password))
+    print("API response: " + response.text)
+    assert response.status_code == expected_code
+
+    if type(expected_data) is dict:
+        processed_response = response.json()
+        assert processed_response is not None
+        actual_keys = processed_response.keys()
+        expected_keys = expected_data.keys()
+        assert all(k in actual_keys for k in expected_keys)
+    else:
+        processed_response = response.text.strip()
+        assert processed_response == expected_data
+
+    return processed_response
