f267b916c6d17ebf6daa39efabf807d87076d926
broodingdark <ivan.ivanov@solarwinds.com>
2021-06-30 15:11:09 +0300

    add config validation

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import sys
+import json
 import argparse
-
+from pathlib import Path
 
 __version__ = "1.0"
+CONFIG_ERROR_MESSAGE = "Invalid config! Please use the default one for reference!"
 
 
 def init_argparse() -> argparse.ArgumentParser:
@@ -20,6 +22,27 @@ def init_argparse() -> argparse.ArgumentParser:
     arg_parser.add_argument("config", nargs="?")
 
     return arg_parser
+
+
+def validate_config(conf_path) -> dict:
+    """
+    Validate provided config file that it is a valid JSON file
+    and contains the expected keys
+    :param conf_path: path to file
+    :return: dict
+    """
+    path_from = Path(conf_path).resolve()
+    with path_from.open("r") as conf:
+        try:
+            config = json.load(conf)
+        except json.decoder.JSONDecodeError:
+            sys.exit(CONFIG_ERROR_MESSAGE)
+
+    expected_keys = ["credentials", "endpoints", "test_data", "expected_responses"]
+    actual_keys = config.keys()
+    assert all(k in actual_keys for k in expected_keys), CONFIG_ERROR_MESSAGE
+
+    return config
 
 
 def main(args):
